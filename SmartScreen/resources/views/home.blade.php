81722d4282153ed20b1b751ceb029dbb262b3ddb
hasan.sidawi <hasan.sidawi@zarcony.com>
2019-07-13 19:03:49 +0300

    home page layout is ready

--- SmartScreen/resources/views/home.blade.php
+++ SmartScreen/resources/views/home.blade.php
@@ -4,24 +4,42 @@
 	<script src="https://static.codepen.io/assets/editor/live/console_runner-1df7d3399bdc1f40995a35209755dcfd8c7547da127f6469fd81e5fba982f6af.js"></script><script src="https://static.codepen.io/assets/editor/live/css_reload-5619dc0905a68b2e6298901de54f73cefe4e079f65a75406858d92924b4938bf.js"></script><meta charset="UTF-8"><meta name="robots" content="noindex"><link rel="shortcut icon" type="image/x-icon" href="https://static.codepen.io/assets/favicon/favicon-aec34940fbc1a6e787974dcd360f2c6b63348d4b1f4e06c77743096d55480f33.ico"><link rel="mask-icon" type="" href="https://static.codepen.io/assets/favicon/logo-pin-8f3771b1072e3c38bd662872f6b673a722f4b3ca2421637d5596661b4e2132cc.svg" color="#111"><link rel="canonical" href="https://codepen.io/Flat-Pixels/pen/qQParK">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 	<link rel="stylesheet" href="{{ url('css/main.css') }}">
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
   
 </head>
   
 <body>
-	
-	<div class="card">
-		<div class="card_part card_part-one">
+	<div class="container">
+	<div class="row p-5" >
+		<div class="col-6" id="frame">
+
+			<div class="card">
+				<div class="card_part card_part-one">
+				</div>
+				
+				<div class="card_part card_part-two">
+				</div>
+				
+				<div class="card_part card_part-three">
+				</div>
+				
+				<div class="card_part card_part-four">
+				</div>
+				
+			</div>
 		</div>
-		
-		<div class="card_part card_part-two">
-		</div>
-		
-		<div class="card_part card_part-three">
-		</div>
-		
-		<div class="card_part card_part-four">
+		<div class="col-6 p-5">
+			<div class="row">
+				<textarea placeholder="This is an awesome comment box" rows="20" name="comment[text]" id="comment_text" cols="40" class="ui-autocomplete-input" autocomplete="off" role="textbox" aria-autocomplete="list" aria-haspopup="true"></textarea>
+			</div>		
 		</div>
 	</div>
+</div>
 </body>
 
 </html>
